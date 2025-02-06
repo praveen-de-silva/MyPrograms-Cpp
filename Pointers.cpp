2a8a4a535fed8aafb76560b54d123b30e7c52896
Praveen De Silva <desilvabkp.23@uom.lk>
2025-02-06 23:56:56 +0530

    Pointers

---
+++ Pointers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>  // Required for cout
+using namespace std;
+
+class Boarding {
+    public:
+        int noMembers;
+        int fee;
+
+        Boarding(int aNoMembers, int aFee) {
+            noMembers = aNoMembers;
+            fee = aFee;
+    }
+
+
+};
+
+class Helper {  
+    public:
+    double perPersonFee(Boarding *brd) {
+        return double(brd -> fee) / (brd -> noMembers);
+    } 
+};
+
+
+    int main() {
+        Boarding brd1(4, 20000);
+        Helper helper;
+
+        cout << helper.perPersonFee(&brd1) << endl;
+        
+        return 0;
+    }
