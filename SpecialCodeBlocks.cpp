8d3bc5864287664d426ee62a5c0645855bf9c8fd
Praveen De Silva <desilvabkp.23@uom.lk>
2025-03-13 02:40:24 +0530

    String to Interger

--- SpecialCodeBlocks.cpp
+++ SpecialCodeBlocks.cpp
@@ -12,11 +12,27 @@ void printArr(vector<int> arr) { // print array elements
 
 int main() {
     /*  
+    ==========================
+    Convert string to interger
+    ==========================
+    */
+
+    // === Method 01 : Using 'stoi' (String TO Integer) ===
+    string numStr = "12";
+    int numInt = stoi(numStr);
+
+    cout << numInt << endl;
+
+
+    /*  
     ================================================
     Initialize a Vector Array filled with a constant
     ================================================
     */
-    vector<int> myArr(5, 0); 
+    // vector<int> myArr(5, 0); 
 
-    printArr(myArr);
+    // printArr(myArr);
+
+
+
 }
