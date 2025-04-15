77ddfb0d6c5d21068e4256b3caa50a1f6dc9fda0
Praveen De Silva <desilvabkp.23@uom.lk>
2025-04-15 19:57:50 +0530

    Refactor Jacobi method functions for improved parameter handling and iteration output

--- S2_Math_NM_JacobiMethod.cpp
+++ S2_Math_NM_JacobiMethod.cpp
@@ -1,40 +1,45 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
-double func1(double x2, double x3) {
-    return (7 - (2*x2 + 3*x3)) / 9;
+double func1(double x2, double x3, double x4) {
+    return (0 - (-50*x2)) / 200;
 }
 
-double func2(double x1, double x3) {
-    return (2 - (1*x1 + 9*x3)) / 12;
+double func2(double x1, double x3, double x4) {
+    return (0 - (50*x1 + 75*x3)) / (-125);
 }
 
-double func3(double x1, double x2) {
-    return (1 - (4*x1 + 6*x2)) / 14;
+double func3(double x1, double x2, double x4) {
+    return (0 - (75*x2 + 225*x4)) / (-300);
+}
+
+double func4(double x1, double x2, double x3) {
+    return (9806.65 - (-225*x3)) / 225;
 }
 
 int main() {
-    double x1 = 0, x2 = 0, x3 = 0;
-    double x1_prev = 0, x2_prev = 0, x3_prev = 0;
+    double x1 = 65.5, x2 = 261.5, x3 = 392.5, x4 = 435.5;
+    double norm = 0, norm_prev = 0;
     
-    for (int i=0; i<20; i++) {
-        cout << "=== Iteration : " << i << " ==="
-             << "\nx1 : " << x1 << ", Error : " << (x1 - x1_prev) / x1 
-             << "\nx2 : " << x2 << ", Error : " << (x2 - x2_prev) / x2 
-             << "\nx3 : " << x3 << ", Error : " << (x3 - x3_prev) / x3 << endl;
+    for (int i=1; i<1000; i++) {
+        norm = max(abs(x1), max(abs(x2),  max(abs(x3), abs(x4))));
         
-        x1_prev = x1;
-        x2_prev = x2;
-        x3_prev = x3;
-             
-        x1 = func1(x2, x3);
-        x2 = func2(x1, x3);
-        x3 = func3(x1, x2);
+        cout << "=== Iteration : " << i << " ==="
+             << "\nx1 : " << x1 
+             << "\nx2 : " << x2 
+             << "\nx3 : " << x3
+             << "\nx4 : " << x4 
+             << "\nError : " << abs(norm - norm_prev) / norm << endl;
+                     
+        x1 = func1(x2, x3, x4);
+        x2 = func2(x1, x3, x4);
+        x3 = func3(x1, x2, x4);
+        x4 = func4(x1, x2, x3);
+        norm_prev = norm;
     }
-    
-
     return 0;
 }
